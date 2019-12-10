83097197b74f8518ec79f57f298ff98dbc5c03d0
joban <45963736+abduhaye@users.noreply.github.com>
2019-12-10 23:36:00 +0100

    adding starting project

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from PIL import Image
+# grab first and second argument
+in_folder = sys.argv[1]
+out_folder = sys.argv[2]
+# check if new/ exists, if not create
+if not (os.path.exists(out_folder)):
+    os.makedirs(out_folder)
+for img_name in os.listdir(in_folder):
+     img = Image.open(f'{in_folder}{img_name}')
+     spli_img_name = os.path.splitext(img_name)[0]
+     img.save(f'{out_folder}{spli_img_name}.png','png')
+     print("All Done!")
+
